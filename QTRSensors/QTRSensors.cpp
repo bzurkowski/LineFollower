aec42c9e082fe773075d63134c8ec8e27fe883ba
MaciejGawel <m.gawel@hotmail.com>
2014-12-06 20:59:54 +0100

    Add QTRSensors.cpp and QTRSensors.h

---
+++ QTRSensors/QTRSensors.cpp
@@ -0,0 +1,204 @@
+#include <stdlib.h>
+#include "QTRSensors.h"
+#include <arduino.h>
+
+//KONSTRUKTORY
+
+QTRSensors::QTRSensors() {
+	calibratedMinimumOn = 0;
+	calibratedMaximumOn = 0;
+	calibratedMinimumOff = 0;
+	calibratedMaximumOff = 0;
+	_pins = 0;
+}
+
+QTRSensors::QTRSensors(unsigned char* pins, unsigned char numSamplesPerSensor, unsigned char emitterPin) {
+	calibratedMinimumOn = 0;
+	calibratedMaximumOn = 0;
+	calibratedMinimumOff = 0;
+	calibratedMaximumOff = 0;
+	_pins = 0;
+
+	init(pins, numSamplesPerSensor, emitterPin);
+}
+
+//FUNKCJE
+
+void QTRSensors::init(unsigned char *pins, unsigned char numSamplesPerSensor, unsigned char emitterPin) {
+	int i;
+	calibratedMinimumOn = 0;
+	calibratedMaximumOn = 0;
+	calibratedMinimumOff = 0;
+	calibratedMaximumOff = 0;
+
+	if (_pins == 0) {
+		_pins = unsigned char[NUM_QTR_SENSORS]; //TODO zmienic na malloc lub wrzucic do .h
+		if (_pins == 0) return;
+	}
+
+	for (i = 0; i < NUM_QTR_SENSORS; i++)
+		_pins[i] = pins[i];
+
+	_emitterPin = emitterPin;
+	_numSamplesPerSensor = numSamplesPerSensor;
+	_maxValue = 1023;
+}
+
+void QTRSensors::read(unsigned int *sensor_values, unsigned char readMode = QTR_EMITTERS_ON) {
+	unsigned int off_values[NUM_QTR_SENSORS];
+	int i;
+
+	if (readMode == QTR_EMITTERS_ON || readMode == QTR_EMITTERS_ON_AND_OFF)
+		emittersOn();
+	else
+		emittersOff();
+
+	readPrivate(sensor_values);
+	emittersOff();
+
+	if (readMode == QTR_EMITTERS_ON_AND_OFF) {
+		readPrivate(off_values);
+
+		for(i = 0; i < NUM_QTR_SENSORS, i++) {
+			sensor_values[i] += _maxValue - off_values[i];
+		}
+	}
+}
+
+void QTRSensors::emittersOff() {
+	if (_emitterPin == QTR_NO_EMITTER_PIN) return; //jezeli nie ustawimy LEDON'a to nie mozemy wylaczyc emiterow
+
+	pinMode(_emitterPin, OUTPUT);
+	digitalWtire(_emitterPin, LOW);
+	delayMicroseconds(200);
+}
+
+void QTRSensors::emittersOn() {
+	if (_emitterPin == QTR_NO_EMITTER_PIN) return; //jezeli nie ustawimy LEDON'a to nie mozemy wlaczyc emiterow
+
+	pinMode(_emitterPin, OUTPUT);
+	digitalWtire(_emitterPin, HIGH);
+	delayMicroseconds(200);
+}
+
+void QTRSensors::calibrate(unsigned char readMode) {
+	if (readMode == QTR_EMITTERS_ON || readMode == QTR_EMITTERS_ON_AND_OFF) {
+		calibrateOnOrOff(&calibratedMinimumOn, &calibratedMaximumOn, QTR_EMITTERS_ON);
+	}
+	if (readMode == QTR_EMITTERS_OFF || readMode == QTR_EMITTERS_ON_AND_OFF) {
+		calibrateOnOrOff(&calibratedMinimumOff, &calibratedMaximumOff, QTR_EMITTERS_OFF);
+	}
+}
+
+// void resetCalibration();
+void QTRSensors::readCalibrated(unsigned int *sensor_values, unsigned char readMode) {
+	int i;
+
+	//jezeli nie sa zaalokowane to wracamy
+	if(readMode == QTR_EMITTERS_ON || readMode == QTR_EMITTERS_ON_AND_OFF)
+		if(!calibratedMinimumOn || !calibratedMaximumOn)
+			return;
+	if(readMode == QTR_EMITTERS_OFF || readMode == QTR_EMITTERS_ON_AND_OFF)
+		if(!calibratedMinimumOff || !calibratedMaximumOff)
+			return;
+
+	//jezeli sa to czytamy
+	read(sensor_values, readMode);
+
+	//i poprawiamy
+	for (i = 0; i < NUM_QTR_SENSORS; i++) {
+		unsigned int calmin, calmax;
+		unsigned int denominator;
+
+		if (readMode == QTR_EMITTERS_ON) {
+			calmin = calibratedMinimumOn[i];
+			calmax = calibratedMaximumOn[i];
+		}
+		else if (readMode == QTR_EMITTERS_OFF) {
+			calmin = calibratedMinimumOff[i];
+			calmax = calibratedMaximumOff[i];
+		}
+		else { //if (readMode == QTR_EMITTERS_ON_AND_OFF)
+			if(calibratedMinimumOff[i] < calibratedMinimumOn[i]) // male znaczenie sygnalu
+				calmin = _maxValue;
+			else
+				calmin = calibratedMinimumOn[i] + _maxValue - calibratedMinimumOff[i];
+
+			if(calibratedMaximumOff[i] < calibratedMaximumOn[i]) // male znaczenie sygnalu
+				calmax = _maxValue;
+			else
+				calmax = calibratedMaximumOn[i] + _maxValue - calibratedMaximumOff[i];
+		}
+		
+		denominator = calmax - calmin;
+
+		int x = 0;
+		if (denominator != 0)
+			x = (sensor_values[i] - calmin) * 1000 / denominator;
+		if (x < 0)
+			x = 0;
+		else if(x > 1000)
+			x = 1000;
+
+		sensor_values[i] = x;
+	}
+}
+
+void QTRSensors::readPrivate(unsigned int *sensor_values) {
+	int i, j;
+
+	if(_pins == 0) return;
+
+	for (i = 0; i < NUM_QTR_SENSORS; i++)
+		sensor_values[i] = 0;
+
+	for (i = 0; i < _numSamplesPerSensor; i++)
+		for (j = 0; j < NUM_QTR_SENSORS; j++)
+			sensor_values[j] += analogRead(_pins[i])
+
+	for (i = 0; i < NUM_QTR_SENSORS; i++)
+		sensor_values[i] = sensor_values[i] / _numSamplesPerSensor; //sensor_values[i] = (sensor_values[i] + (_numSamplesPerSensor >> 1)) / _numSamplesPerSensor;
+}
+
+void QTRSensors::calibrateOnOrOff(unsigned int **calibratedMinimum, unsigned int **calibratedMaximum, unsigned char readMode) {
+	int i;
+	unsigned int sensor_values[];
+	unsigned int max_sensor_values[];
+	unsigned int min_sensor_values[];
+
+	//szukanie min i max na czujnikach przez 10 odczytow
+	for (i = 0; i < 10; i++) {
+		read(sensor_values, readMode);
+
+		for (j = 0; j < NUM_QTR_SENSORS; j++) {
+			if (i == 0 || max_sensor_values[j] < sensor_values[j])
+				max_sensor_values[j] = sensor_values[j];
+
+		for (j = 0; j < NUM_QTR_SENSORS; j++) {
+			if (i == 0 || min_sensor_values[j] > sensor_values[j])
+				min_sensor_values[j] = sensor_values[j];
+		}
+	}
+
+	for (i = 0; i < NUM_QTR_SENSORS; i++) {
+		if(min_sensor_values[i] > (*calibratedMaximum)[i])
+			(*calibratedMaximum)[i] = min_sensor_values[i];
+		if(max_sensor_values[i] < (*calibratedMaximum)[i])
+			(*calibratedMinimum)[i] = max_sensor_values[i];
+	}
+}
+
+//DESTRUKTOR
+
+QTRSensors::~QTRSensors() {
+	// if (_pins)
+	// 	free(_pins);
+	if (calibratedMinimumOn)
+		free(calibratedMinimumOn);
+	if (calibratedMaximumOn)
+		free(calibratedMaximumOn);
+	if (calibratedMinimumOff)
+		free(calibratedMinimumOff);
+	if (calibratedMaximumOff)
+		free(calibratedMaximumOff);
+}
---
+++ QTRSensors/QTRSensors.h
@@ -0,0 +1,59 @@
+/*
+	Mniejsze wartości sensorów odpowiadają większemu odbiciu (np. kolor biały)
+	natomiast większe odpowiadają mniejszemu odbiciu (np. kolor czarny).
+	Sensory zwracają wartość pomiędzy 0 a 1023.
+*/
+
+#ifndef QTRSensors.h
+#define QTRSensors.h
+
+//tryb czytania
+#define QTR_EMITTERS_OFF 0 //mierzenie przy wylaczonych diodach IR (odczyt reprezentuje otaczajacy poziom swiatla w poblizu sensorow)
+#define QTR_EMITTERS_ON 1 //mierzenie przy wlaczonych diodach IR (odczyt odbicia)
+#define QTR_EMITTERS_ON_AND_OFF 2 //odczyt w obu stanach
+
+#define NUM_QTR_SENSORS 6 //liczba sensorow
+
+#define QTR_NO_EMITTER_PIN 255 //jezeli emiter pin nie jest ustawiony
+
+class QTRSensors {
+public:
+// konstruktory
+	QTRSensors(); // jezeli uzywamy tego konstruktora to najpierw trzeba wywolac init
+	QTRSensors(unsigned char *pins, unsigned char numSamplesPerSensor = 4, unsigned char emitterPin = QTR_NO_EMITTER_PIN);
+
+//funkcje
+	void init(unsigned char *pins, unsigned char numSamplesPerSensor = 4, unsigned char emitterPin = QTR_NO_EMITTER_PIN);
+
+	void read(unsigned int *sensor_values, unsigned char readMode = QTR_EMITTERS_ON);
+	void emittersOff();
+	void emittersOn();
+
+	void calibrate(unsigned char readMode = QTR_EMITTERS_ON);
+// void resetCalibration();
+	void readCalibrated(unsigned int *sensor_values, unsigned char readMode = QTR_EMITTERS_ON);
+
+//atrybuty
+	unsigned int[NUM_QTR_SENSORS] calibratedMinimumOn;
+	unsigned int[NUM_QTR_SENSORS] calibratedMaximumOn;
+	unsigned int[NUM_QTR_SENSORS] calibratedMinimumOff;
+	unsigned int[NUM_QTR_SENSORS] calibratedMaximumOff;
+
+//destruktor
+	~QTRSensors();
+
+protected:
+
+	unsigned char *_pins;
+	unsigned char _emitterPin;
+	unsigned int _maxValue;
+
+private:
+
+	void readPrivate(unsigned int *sensor_values);
+	void calibrateOnOrOff(unsigned int **calibratedMinimum, unsigned int **calibratedMaximum, unsigned char readMode);
+
+	unsigned char _numSamplesPerSensor;
+};
+
+#endif
